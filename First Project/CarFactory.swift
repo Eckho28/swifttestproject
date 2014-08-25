4288384d364671081c3ac77dfaa5a7950e4c9921
eckho28 <eckho28@gmail.com>
2014-08-25 16:33:13 -0400

    initial commit

---
+++ First Project/CarFactory.swift
@@ -0,0 +1,17 @@
+//
+//  CarFactory.swift
+//  First Project
+//
+//  Created by Stephen Foung on 8/25/14.
+//  Copyright (c) 2014 Stephen Foung. All rights reserved.
+//
+
+import Foundation
+
+
+class CarFactory {
+    var name = ""
+    var color = ""
+    var horsepower = 0
+    var automaticOption = false
+}
