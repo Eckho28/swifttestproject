2b081a134cfb95a9419ef190c9bd6e9537bd445f
eckho28 <eckho28@gmail.com>
2014-08-25 16:15:52 -0400

    Initial Commit

---
+++ First Project/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  First Project
+//
+//  Created by Stephen Foung on 8/25/14.
+//  Copyright (c) 2014 Stephen Foung. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
